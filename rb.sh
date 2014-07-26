e69ae383cb719e7ba5b792ab837e14216d0f7c67
Raymond Bourges <raymond.bourges@univ-rennes1.fr>
2014-07-26 08:44:20 +0200

    Utilisation du master de oauth-ng.js pour gestion du state

---
+++ rb.sh
@@ -0,0 +1,4 @@
+cd /opt/A_GARDER_UN_PEU/projets/apis/apis-example-resource-server
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 -jar target/apis-example-resource-server-1.3.5.jar
+cd -
+
